27c1d3bc52f6039996a7ef5166c1369d2662bec1
Mauricio <u202211572@upc.edu.pe>
2024-05-31 08:21:02 -0500

    feat(communication): deleted unnecessary methods in notification query service.

--- sweetmanager.API/communication/Domain/Services/INotificationQueryService.cs
+++ sweetmanager.API/communication/Domain/Services/INotificationQueryService.cs
@@ -5,7 +5,5 @@ namespace sweetmanager.API.communication.Domain.Services;
 
 public interface INotificationQueryService
 {
-    Task<Notification> Handle(CreateNotificationQuery query);
-    
     Task<IEnumerable<Notification>> Handle(GetAllNotificationsQuery query);
 }
