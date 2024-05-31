74f2be6be77823f9a3d642b73d926f01590c9d5b
Mauricio <u202211572@upc.edu.pe>
2024-05-31 08:19:23 -0500

    chore(communication): deleted unnecessary query.

--- sweetmanager.API/communication/Domain/Model/Queries/CreateNotificationQuery.cs
+++
@@ -1,5 +0,0 @@
-using sweetmanager.API.communication.Domain.Model.Aggregates;
-
-namespace sweetmanager.API.communication.Domain.Model.Queries;
-
-public record CreateNotificationQuery(Notification newNotificationSource);
