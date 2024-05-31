b296835c6fbfe892c536e17ac32ac4f4a963708b
Mauricio <u202211572@upc.edu.pe>
2024-05-31 08:09:26 -0500

    feat(communication): added notification query service.

--- sweetmanager.API/communication/Domain/Services/INotificationQueryService.cs
+++ sweetmanager.API/communication/Domain/Services/INotificationQueryService.cs
@@ -1,6 +1,11 @@
+using sweetmanager.API.communication.Domain.Model.Aggregates;
+using sweetmanager.API.communication.Domain.Model.Queries;
+
 namespace sweetmanager.API.communication.Domain.Services;
 
 public interface INotificationQueryService
 {
+    Task<Notification> Handle(CreateNotificationQuery query);
     
+    Task<IEnumerable<Notification>> Handle(GetAllNotificationsQuery query);
 }
