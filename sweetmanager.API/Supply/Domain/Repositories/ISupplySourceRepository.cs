3dd0a406c25134add0cf11a2ce9fbcdf75dfd246
LeoShadic345 <leonardoshadow345@gmail.com>
2024-06-01 01:35:39 -0500

    feat(supply): created SupplySource interface repository, and the command and query services

---
+++ sweetmanager.API/Supply/Domain/Repositories/ISupplySourceRepository.cs
@@ -0,0 +1,9 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using sweetmanager.API.Supply.Domain.Model.Aggregates;
+
+namespace sweetmanager.API.Supply.Domain.Repositories;
+
+public interface ISupplySourceRepository : IBaseRepository<SupplySource>
+{
+    Task<SupplySource> GetSupplySourceByIdAsync(int id);
+}
---
+++ sweetmanager.API/Supply/Domain/Services/ISupplySourceCommandService.cs
@@ -0,0 +1,9 @@
+﻿using sweetmanager.API.Supply.Domain.Model.Aggregates;
+using sweetmanager.API.Supply.Domain.Model.Commands;
+
+namespace sweetmanager.API.Supply.Domain.Services;
+
+public interface ISupplySourceCommandService
+{
+    Task<SupplySource> Handle(CreateSupplySourceCommand command);
+}
