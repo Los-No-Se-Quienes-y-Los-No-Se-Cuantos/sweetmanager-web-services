2699424cc00813f9d6df1656b324a3ce8d23eff4
Mauricio <mauriciorojasanchez@gmail.com>
2024-06-01 02:23:18 -0500

    feat(communication): added notification controller and notification resource for information handling.

---
+++ sweetmanager.API/communication/Interfaces/REST/NotificationController.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using sweetmanager.API.communication.Domain.Model.Queries;
+using sweetmanager.API.communication.Domain.Services;
+using sweetmanager.API.communication.Interfaces.REST.Resources;
+using sweetmanager.API.communication.Interfaces.REST.Transform;
+
+namespace sweetmanager.API.communication.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class NotificationController(INotificationCommandService notificationCommandService, INotificationQueryService notificationQueryService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateNotification(CreateNotificationResource resource)
+    {
+        var createNotificationCommand = CreateNotificationCommandFromResourceAssembler.ToCommandFromResource(resource);
+        
+        var notification = await notificationCommandService.Handle(createNotificationCommand);
+        
+        if (notification == null) BadRequest();
+        
+        var notificationResource = NotificationResourceFromEntityAssembler.ToResourceFromEntity(notification);
+
+        return CreatedAtAction(nameof(GetNotificationById), new { notificationId = notification.Id }, notificationResource);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllNotifications()
+    {
+        var getAllNotificationsQuery = new GetAllNotificationsQuery();
+        
+        var notifications = await notificationQueryService.Handle(getAllNotificationsQuery);
+
+        var notificationResources = notifications.Select(NotificationResourceFromEntityAssembler.ToResourceFromEntity);
+
+        return Ok(notificationResources);
+    }
+    
+    
+    [HttpGet("{notificationId:int}")]
+    public async Task<IActionResult> GetNotificationById(int notificationId)
+    {
+        var getNotificationByIdQuery = new GetNotificationByIdQuery(notificationId);
+        
+        var notification = await notificationQueryService.Handle(getNotificationByIdQuery);
+
+        if (notification == null) return NotFound();
+        
+        var notificationResource = NotificationResourceFromEntityAssembler.ToResourceFromEntity(notification);
+
+        return Ok(notificationResource);
+    }
+}
---
+++ sweetmanager.API/communication/Interfaces/REST/Resources/NotificationResource.cs
@@ -0,0 +1,3 @@
+﻿namespace sweetmanager.API.communication.Interfaces.REST.Resources;
+
+public record NotificationResource(string Title, string Message);
