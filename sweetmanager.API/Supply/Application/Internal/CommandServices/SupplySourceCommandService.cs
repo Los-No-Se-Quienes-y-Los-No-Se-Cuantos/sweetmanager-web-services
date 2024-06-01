2efb0023c8bfb07c4b347495812ec8d93c30cc98
LeoShadic345 <leonardoshadow345@gmail.com>
2024-06-01 01:39:34 -0500

    feat(supply): created SupplySource repository, and the command and query services.

---
+++ sweetmanager.API/Supply/Application/Internal/CommandServices/SupplySourceCommandService.cs
@@ -0,0 +1,19 @@
+﻿using sweetmanager.API.Shared.Domain.Repositories;
+using sweetmanager.API.Supply.Domain.Model.Aggregates;
+using sweetmanager.API.Supply.Domain.Model.Commands;
+using sweetmanager.API.Supply.Domain.Repositories;
+using sweetmanager.API.Supply.Domain.Services;
+
+namespace sweetmanager.API.Supply.Application.Internal.CommandServices;
+
+public class SupplySourceCommandService (ISupplySourceRepository supplySourceRepository, IUnitOfWork unitOfWork) 
+    :ISupplySourceCommandService
+{
+    public async Task<SupplySource> Handle(CreateSupplySourceCommand command)
+    {
+        var supplySource  = new SupplySource(command);
+        await supplySourceRepository.AddAsync(new SupplySource(command));
+        await unitOfWork.CompleteAsync();
+        return supplySource;
+    }
+}
---
+++ sweetmanager.API/Supply/Application/Internal/QueryServices/SupplySourceQueryService.cs
@@ -0,0 +1,19 @@
+﻿using sweetmanager.API.Supply.Domain.Model.Aggregates;
+using sweetmanager.API.Supply.Domain.Model.Queries;
+using sweetmanager.API.Supply.Domain.Repositories;
+using sweetmanager.API.Supply.Domain.Services;
+
+namespace sweetmanager.API.Supply.Application.Internal.QueryServices;
+
+public class SupplySourceQueryService(ISupplySourceRepository sourceRepository):  ISupplySourceQueryService
+{
+    public async Task<IEnumerable<SupplySource>> Handle(GetAllSuppliesSourceQuery query)
+    {
+        return await sourceRepository.ListAsync();
+    }
+    public async Task<SupplySource?> Handle(GetSupplySourceByIdQuery query)
+    {
+        return await sourceRepository.GetSupplySourceByIdAsync(query.SupplyId);
+    }
+    
+}
