e15f9e156556e415b575e9c72e2477cbfed9975f
Ryzeon <alex.ramon2004@gmail.com>
2024-06-08 02:24:57 -0500

    feat(payment): create acl for client id fetcher.

--- sweetmanager.API/Clients/Interfaces/ACL/IClientContextFacade.cs
+++ sweetmanager.API/Clients/Interfaces/ACL/IClientContextFacade.cs
@@ -1,8 +1,10 @@
-﻿namespace sweetmanager.API.Clients.Interface.ACL
+﻿namespace sweetmanager.API.Clients.Interfaces.ACL
 {
     public interface IClientsContextFacade
     {
         Task<int> CreateClient(int id, string name, string lastName, int age, string genre, int phone, string email, string state);
         Task<int> FetchClientById(int id);
+        
+        Task<int> FetchClientByEmail(string email);
     }
 }
---
+++ sweetmanager.API/Payments/Domain/Model/Aggregates/Payment.cs
@@ -0,0 +1,35 @@
+using sweetmanager.API.Payments.Domain.Model.Command;
+using sweetmanager.API.Payments.Domain.Model.ValueObjects;
+
+namespace sweetmanager.API.Payments.Domain.Model.Aggregates;
+
+public partial class Payment
+{
+    public int Id { get; private set; }
+
+    public PaymentCardInfo CardInfo { get; private set; }
+
+    public string Email { get; private set; }
+
+    public ProfileId ProfileId { get; private set; }
+
+    public decimal Amount { get; private set; }
+
+    public Payment(CreatePaymentCommand command)
+    {
+        this.CardInfo =
+            new PaymentCardInfo(command.CardNumber, command.CardHolderName, command.ExpiryDate, command.Cvv);
+        this.Email = command.ProfileEmail;
+        this.ProfileId = new ProfileId(0);
+        this.Amount = command.Amount;
+    }
+
+    public Payment(CreatePaymentCommand command, int profileId)
+    {
+        this.CardInfo =
+            new PaymentCardInfo(command.CardNumber, command.CardHolderName, command.ExpiryDate, command.Cvv);
+        this.Email = command.ProfileEmail;
+        this.ProfileId = new ProfileId(profileId);
+        this.Amount = command.Amount;
+    }
+}
