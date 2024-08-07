2b142adecfc6ff6cc69aff83b86547f465c6d5e5
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:24:32 -0500

    feat: added UserCredential to storage sensitive information.

---
+++ sweetmanager.API/IAM/Domain/Model/Entities/Credential/UserCredential.cs
@@ -0,0 +1,39 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Aggregates;
+using sweetmanager.API.IAM.Domain.Model.Commands;
+
+namespace sweetmanager.API.IAM.Domain.Model.Entities;
+
+
+public sealed partial class UserCredential
+{
+    public int Id { get; private set; }
+    
+    public int UserId { get; private set;  }
+
+    public string Argon2IdUserHash { get; private set; }
+
+    public User User { get; private set; } = null!;
+
+    public UserCredential()
+    {
+        this.Id = 0;
+        
+        this.UserId = 0;
+        
+        this.Argon2IdUserHash = string.Empty;
+    }
+
+    public UserCredential(int userId, string argon2IdUserHash)
+    {
+        this.UserId = userId;
+        
+        this.Argon2IdUserHash = argon2IdUserHash;
+    }
+
+    public UserCredential(CreateUserCredentialCommand command)
+    {
+        this.UserId = command.UserId;
+        
+        this.Argon2IdUserHash = command.Argon2IdUserHash;
+    }
+}
