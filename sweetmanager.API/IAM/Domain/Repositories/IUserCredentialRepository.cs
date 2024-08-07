46047b7a322d35e8b43ac5aba6e828de46503eca
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:42:35 -0500

    added: added UserCredential repository service.

---
+++ sweetmanager.API/IAM/Domain/Repositories/IUserCredentialRepository.cs
@@ -0,0 +1,9 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Entities;
+using sweetmanager.API.Shared.Domain.Repositories;
+
+namespace sweetmanager.API.IAM.Domain.Repositories;
+
+public interface IUserCredentialRepository : IBaseRepository<UserCredential>
+{
+    Task<UserCredential?> FindByUserIdAsync(int userId);
+}
