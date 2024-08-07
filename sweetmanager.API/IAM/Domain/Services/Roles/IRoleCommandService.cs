ce1199d36041273fa8b6d6c77b0b87632d465eb1
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:32:25 -0500

    feat: added Role command and queries services.

---
+++ sweetmanager.API/IAM/Domain/Services/Roles/IRoleCommandService.cs
@@ -0,0 +1,10 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Commands;
+
+namespace sweetmanager.API.IAM.Domain.Services.Roles;
+
+using Model.Entities;
+
+public interface IRoleCommandService
+{
+    Task<Role?> Handle(SeedRolesCommand command);
+}
---
+++ sweetmanager.API/IAM/Domain/Services/Roles/IRoleQueryService.cs
@@ -0,0 +1,12 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Entities;
+
+using sweetmanager.API.IAM.Domain.Model.Queries;
+
+namespace sweetmanager.API.IAM.Domain.Services.Roles;
+
+public interface IRoleQueryService
+{
+    Task<IEnumerable<Role>> Handle(GetAllRolesQuery query);
+
+    Task<Role?> Handle(GetRoleByNameQuery query);
+}
