eaca6ac7e056f6343602cc89f328cd968e65288d
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:43:42 -0500

    added: added RequestAuthorizationMiddleware for authorized requests.

---
+++ sweetmanager.API/IAM/Infrastructure/Pipeline/Middleware/Components/RequestAuthorizationMiddleware.cs
@@ -0,0 +1,49 @@
+using sweetmanager.API.IAM.Application.Internal.OutboundContext;
+using sweetmanager.API.IAM.Domain.Model.Queries;
+using sweetmanager.API.IAM.Domain.Services;
+using sweetmanager.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+
+namespace sweetmanager.API.IAM.Infrastructure.Pipeline.Middleware.Components;
+
+public class RequestAuthorizationMiddleware(RequestDelegate next, ILogger<RequestAuthorizationMiddleware>logger)
+{
+    public async Task InvokeAsync(HttpContext context, IUserQueryService userQueryService, ITokenService tokenService)
+    {
+        var endpoint = context.Request.HttpContext.GetEndpoint();
+        
+        var allowAnonymous =
+            context.Request.HttpContext.GetEndpoint()!.Metadata.Any(m =>
+                m.GetType() == typeof(AllowAnonymousAttribute));
+        
+        logger.LogInformation($"Endpoint: {endpoint?.DisplayName}, AllowAnonymous: {allowAnonymous}");
+        
+        if (allowAnonymous)
+        {
+            await next(context);
+
+            return;
+        }
+        
+        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+        var tokenResult = tokenService.ValidateToken(token) ?? throw new Exception("Invalid Token!");
+
+        dynamic? validation = null;
+        
+        // Only if I have more than 1 Aggregate 
+        /*if (tokenResult.Role == "ROLE_MANAGER")
+            validation = await userQueryService.Handle(new GetUserByIdQuery(tokenResult.Id));
+        
+        else if(tokenResult.Role == "ROLE_WORKER")
+            validation = await userQueryService.Handle(new GetUserByIdQuery(tokenResult.Id));*/
+
+        validation = await userQueryService.Handle(new GetUserByIdQuery(tokenResult.Id));
+
+        if (validation is null)
+            throw new Exception("Invalid credentials!");
+
+        context.Items["Credentials"] = tokenResult;
+        
+        await next(context);
+    }
+}
