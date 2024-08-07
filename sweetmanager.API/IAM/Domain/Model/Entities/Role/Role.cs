4080d7fb8fcf6686a27a598e8fb667f7a53dfe40
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:30:38 -0500

    feat: added Role entity for role system.

---
+++ sweetmanager.API/IAM/Domain/Model/Entities/Role/Role.cs
@@ -0,0 +1,16 @@
+﻿using sweetmanager.API.IAM.Domain.Model.ValueObjects;
+
+namespace sweetmanager.API.IAM.Domain.Model.Entities;
+
+public partial class Role(ERoles name)
+{
+    
+    public long Id { get; private set; }
+    
+    public ERoles Name { get; private set; } = name;
+
+    public string GetStringName() => Name.ToString();
+    
+    public static Role GetDefaultRole() =>  new Role(ERoles.ROLE_MANAGER);
+    
+}
