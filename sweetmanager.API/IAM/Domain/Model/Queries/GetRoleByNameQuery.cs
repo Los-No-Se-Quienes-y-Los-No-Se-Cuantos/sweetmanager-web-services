ec14914916d8faa10a642b9c48dccb9ff3f4e04a
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:55:03 -0500

    feat: added GetRoleByNameQuery for Role entity.

---
+++ sweetmanager.API/IAM/Domain/Model/Queries/GetRoleByNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Exceptions;
+using sweetmanager.API.IAM.Domain.Model.ValueObjects;
+
+namespace sweetmanager.API.IAM.Domain.Model.Queries;
+
+public record GetRoleByNameQuery(ERoles Name)
+{
+    
+}
