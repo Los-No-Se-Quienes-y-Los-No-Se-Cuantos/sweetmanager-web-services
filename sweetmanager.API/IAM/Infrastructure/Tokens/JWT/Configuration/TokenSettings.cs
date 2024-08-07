aa62f4176d71b489f635b59c2e3e5948bdfe950e
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:20:45 -0500

    feat: updated token settings.

--- sweetmanager.API/IAM/Infrastructure/Tokens/JWT/Configuration/TokenSettings.cs
+++ sweetmanager.API/IAM/Infrastructure/Tokens/JWT/Configuration/TokenSettings.cs
@@ -3,4 +3,18 @@ namespace sweetmanager.API.IAM.Infrastructure.Tokens.JWT.Configuration;
 public class TokenSettings
 {
     public string Secret { get; set; }
+    
+    public string Audience { get; set; }
+    
+    public string Issuer { get; set; }
+    
+    public int ExpirationInMinutes { get; set; }
+
+    public TokenSettings()
+    {
+        Secret = string.Empty;
+        Audience = string.Empty;
+        Issuer = string.Empty;
+        ExpirationInMinutes = 0;
+    }
 }
