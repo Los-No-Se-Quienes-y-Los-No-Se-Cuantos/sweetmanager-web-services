139fa78eacdd6d89b23d10297ca20d9c52c47cf9
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:52:22 -0500

    feat: added authorization to role controller.

---
+++ sweetmanager.API/IAM/Interfaces/REST/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using sweetmanager.API.IAM.Domain.Model.Queries;
+using sweetmanager.API.IAM.Domain.Services.Roles;
+using sweetmanager.API.IAM.Interfaces.REST.Transform;
+
+namespace sweetmanager.API.IAM.Interfaces.REST;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class RoleController(IRoleQueryService roleQueryService) : ControllerBase
+{
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllRoles()
+    {
+        try
+        {
+            var roles = await roleQueryService.Handle(new GetAllRolesQuery());
+
+            var roleResources = roles.Select(RoleResourceFromEntityAssembler.ToResourceFromEntity);
+
+            return Ok(roleResources);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
