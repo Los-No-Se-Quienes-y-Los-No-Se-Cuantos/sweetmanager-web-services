49711ce3ed074af9f17aceba19b28a1bc9d2a120
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:28:15 -0500

    feat: added UserCredential command and queries services.

---
+++ sweetmanager.API/IAM/Domain/Services/UserCredentials/IUserCredentialCommandService.cs
@@ -0,0 +1,8 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Commands;
+
+namespace sweetmanager.API.IAM.Domain.Services.UserCredentials;
+
+public interface IUserCredentialCommandService
+{
+    Task<bool> Handle(CreateUserCredentialCommand command);
+}
---
+++ sweetmanager.API/IAM/Domain/Services/UserCredentials/IUserCredentialQueryService.cs
@@ -0,0 +1,9 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Entities;
+using sweetmanager.API.IAM.Domain.Model.Queries;
+
+namespace sweetmanager.API.IAM.Domain.Services.UserCredentials;
+
+public interface IUserCredentialQueryService
+{
+    Task<UserCredential?> Handle(GetUserCredentialByUserIdQuery query);
+}
