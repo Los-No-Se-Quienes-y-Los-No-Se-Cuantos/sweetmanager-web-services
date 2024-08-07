98903c6a3bef478804d7c21ad0bc946687094dd1
Mauricio <mauriciorojasanchez@gmail.com>
2024-08-07 00:49:28 -0500

    feat: added service to pre-populate database.

---
+++ sweetmanager.API/IAM/Infrastructure/Poblation/Roles/DatabaseInitializer.cs
@@ -0,0 +1,23 @@
+﻿using sweetmanager.API.IAM.Domain.Model.Commands;
+using sweetmanager.API.IAM.Domain.Model.Queries;
+using sweetmanager.API.IAM.Domain.Services.Roles;
+
+namespace sweetmanager.API.IAM.Infrastructure.Poblation.Roles;
+
+public class DatabaseInitializer(IRoleCommandService roleCommandService, IRoleQueryService
+    roleQueryService)
+{
+    public async Task InitializeAsync()
+    {
+        // Check if the Role table is empty
+
+        var result = 
+            await roleQueryService.Handle(new GetAllRolesQuery());
+        
+        if (!result.Any())
+        {
+              // Prepopulate the Role table
+              await roleCommandService.Handle(new SeedRolesCommand());
+        }
+    }
+}
